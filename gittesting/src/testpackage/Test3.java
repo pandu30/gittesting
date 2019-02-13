8221f28b03a709ecf07a4ebffdf328773e8e2e48
pandu30 <42614942+pandu30@users.noreply.github.com>
2019-02-13 21:03:54 +0500

    Update Test3.java

--- gittesting/src/testpackage/Test3.java
+++ gittesting/src/testpackage/Test3.java
@@ -6,7 +6,24 @@ public class Test3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		{
+		if(l.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver","D:\\Pandu\\Selenium\\chromedriver.exe");
+			driver=new ChromeDriver();
+			
+		}
+		elseif(l.equalsIgnoreCase("Firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver","D:\\Pandu\\Selenium\\geckodriver.exe");
+			driver=new FirefoxDriver();
+		}
+		else
+		{
+			System.setProperty("webdriver.ie.driver","D:\\Pandu\\Selenium\\iedriveraserver.exe");
+			driver=new InternetExplorerDriver();
+		}
+	}
 
 	}
 
